b2144e491b0622af00a45195170788c57e2220f3
João Rocha <joao.rocha@bynd.com>
2023-06-23 16:58:26 +0100

    Added Transcend's Button and MUI's CircularProgress

--- src/components/pages/HomePage/index.tsx
+++ src/components/pages/HomePage/index.tsx
@@ -1,11 +1,22 @@
 import Link from 'next/link';
 import React from 'react';
+import { Button } from '@bynd-ui-library/react';
+import { CircularProgress } from '@mui/material';
 
 export const HomePage = () => {
+
+  const handleToggle = () => {
+    console.log('Clicked button');
+  }
+
   return (
     <div>
       <h1 className="title">Welcome!</h1>
       <h2> Go to <Link href="/todoapp">To-do App</Link></h2>
+      <div>
+        <Button onClick={handleToggle}>BYND button</Button>
+        <CircularProgress color="inherit" className="hidden"/>
+      </div>
     </div>
   );
 }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,12 +2,15 @@ import '@/styles/globals.scss'
 import '@bynd-ui-library/react/dist/style.css';
 
 import type { AppProps } from 'next/app'
+import { ByndUIProvider, themes } from '@bynd-ui-library/react';
 
 export default function App({ Component, pageProps }: AppProps) {
 
   return (
-    <div className="container">
-      <Component {...pageProps} />
-    </div>
+    <ByndUIProvider theme={themes.baseTheme}>
+      <div className="container">
+        <Component {...pageProps} />
+      </div>
+    </ByndUIProvider>
   )
 }
